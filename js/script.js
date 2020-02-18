34a9d598bd5546260519b2b736711e103671b365
Kacie Cooper <kcrouch@pdx.edu>
2020-02-18 13:23:23 -0800

    adding effects to homepage

--- js/script.js
+++ js/script.js
@@ -2,35 +2,33 @@
 $(function() {
 // Your Code from here on down. Don't delete that line above!
   
-
-		// init
-		var controller = new ScrollMagic.Controller({
-			globalSceneOptions: {
-				triggerHook: 'onLeave',
-				duration: "200%" // this works just fine with duration 0 as well
-				// However with large numbers (>20) of pinned sections display errors can occur so every section should be unpinned once it's covered by the next section.
-				// Normally 100% would work for this, but here 200% is used, as Panel 3 is shown for more than 100% of scrollheight due to the pause.
-			}
-		});
-
-		// get all slides
-		var slides = document.querySelectorAll("section.panel");
-
-		// create scene for every slide
-		for (var i=0; i<slides.length; i++) {
-			new ScrollMagic.Scene({
-					triggerElement: slides[i]
-				})
-				.setPin(slides[i], {pushFollowers: false})
-				.addIndicators() // add indicators (requires plugin)
-				.addTo(controller);
-		}
-	});
-
-
-
-
-
+var tea tin = document.querySelector(".tea-tin");
+    var mint1 = document.querySelector(".mint-1");
+    var mint2 = document.querySelector(".mint-2");
+    var mint3 = document.querySelector(".mint-3");
+    var intro = document.querySelector(".intro");
+     
+    function setTranslate(xPos, yPos, el) {
+        el.style.transform = "translate3d(" + xPos + ", " + yPos + "px, 0)";
+    }
+ 
+    window.addEventListener("DOMContentLoaded", scrollLoop, false);
+ 
+    var xScrollPosition;
+    var yScrollPosition;
+ 
+    function scrollLoop() {
+        xScrollPosition = window.scrollX;
+        yScrollPosition = window.scrollY;
+ 
+        setTranslate(0, yScrollPosition * -0.5, tea tin);
+        setTranslate(0, yScrollPosition * -0.9, mint1);
+        setTranslate(0, yScrollPosition * -1.3, mint2);
+        setTranslate(0, yScrollPosition * -1.7, mint3);
+        setTranslate(0, yScrollPosition * -1.7, intro);
+ 
+        requestAnimationFrame(scrollLoop);
+    }
 
 // End of Your Code . Don't delete that line below!!
 });
